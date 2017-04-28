55b25fc441e0efe7c2d347a582ed7b697778ee1b
Jacob <jacoboakley21@gmail.com>
2017-04-28 01:05:56 -0400

    finished Github Card app

--- beginner/gettingStarted/src/GithubLookup.jsx
+++ beginner/gettingStarted/src/GithubLookup.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-
-
-/*
-
-componentDidMount() {
-  fetch('URL')
-    .then(res => res.json())
-    .then(user => {
-      this.setState({ user })
-    })
-}
-
-*/
+import axios from 'axios'
 
 
 const Card = (props) => {
   return (
     <div style={{margin: '1em'}}>
-      <img width="75" src={props.avatar_url} alt={props.name}/>
+      <img width='75' src={props.avatar_url} alt='user'/>
       <div style={{display: 'inline-block', marginLeft: 10}}>
         <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
         <div>{props.company}</div>
@@ -29,49 +17,62 @@ const Card = (props) => {
 const CardList = (props) => {
   return (
     <div>
-      {props.cards.map(card => <Card {...card} />)}
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
     </div>
   )
 }
 
 class Form extends React.Component {
-  state = {userName: ''}
+  constructor(props) {
+  super(props);
+  
+  this.state = {userName: ''};  
+  }
+  
   handleSubmit = (event) => {
     event.preventDefault();
     console.log('Event: Form Submit', this.state.userName);
-    
-  };
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    .then(resp => {
+      this.props.onSubmit(resp.data);
+      this.setState({ userName: '' });
+    });
+  };  
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" 
+        <input type='text'
                value={this.state.userName}
-               onChange={(event) => this.setState({userName: event.target.value})}
-               placeholder="Github username" required />
-        <button type="submit">Add Card</button>
+               onChange={(event) => this.setState({ userName: event.target.value})}
+               placeholder='Github username' required />
+        <button type='submit'>Add Card</button>
       </form>
-    );
+    )
   }
 }
 
 class GithubLookup extends React.Component {
-  state = {
-    cards: [
-      { name: "Paul O'Shannessy",
-        avatar_url: "https://avatars2.githubusercontent.com/u/8445?v=3",
-        company: "Facebook"},
-      { name: "Paul O'Shannessy",
-        avatar_url: "https://avatars2.githubusercontent.com/u/8445?v=3",
-        company: "Facebook"},
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    }
   }
+  
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+  
   render() {
     return (
-      <div className="content">
-        <Form />
+      <div>
+        <Form onSubmit={this.addNewCard}/>
         <CardList cards={this.state.cards} />
       </div>
-    );
+    )
   }
 }
 
